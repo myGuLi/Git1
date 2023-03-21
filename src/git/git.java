2be62a2bc89676d87f22d59bca15b89d4ae8ddb8
Guli <503033477@qq.com>
2023-03-21 20:16:06 +0800

    1st hot-fix

--- src/git/git.java
+++ src/git/git.java
@@ -5,7 +5,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
-
+        System.out.println("hot-fix 1");
 
     }
 }
